fabe5bc9c7d4ee19cd91fa650857d97dbf5dddf6
Vinay Sangwan <73075190+vinay4194@users.noreply.github.com>
2021-08-10 19:17:20 +0530

    removed api keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,5 @@
 import axios from "axios";
 
-// const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-
-// const options = {
-// 	params: {
-// 		bl_latitude: "11.847676",
-// 		tr_latitude: "12.838442",
-// 		bl_longitude: "109.095887",
-// 		tr_longitude: "109.149359",
-// 	},
-// 	headers: {
-// 		"x-rapidapi-key": "338326bf77msheb219c9a236b1aep1e553ejsn650084546ed2",
-// 		"x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-// 	},
-// };
-
 export const getPlacesData = async (type, sw, ne) => {
 	try {
 		const {
